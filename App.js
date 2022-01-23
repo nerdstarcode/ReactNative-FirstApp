9f508a58edb7d59b162ce492e1a21c35b303eb90
nerdstarcode <sthivendev@gmail.com>
2022-01-23 12:53:45 -0300

    iniciando meu primeiro app

--- App.js
+++ App.js
@@ -5,8 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Meu primeiro app em react-native</Text>
+      <StatusBar style="light" />
+      <View style={styles.card}>
+      <Text style={styles.cardTitle}>Reactive Native</Text>
+      <Text style={styles.cardText}>Uma ferramenta bem interessante para criação de apps, o expo ajuda muito a desenvolvelos na falta de um pc mais parrudo</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +18,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#2a2a2a',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  title: {
+    color: 'white',
+    marginBottom: 24,
+  },
+  card: {
+    backgroundColor: '#cc01ff',
+    padding: 20,
+    borderRadius: 50,
+  },
+  cardTitle: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cardText: {
+    color: 'white',
+
+  }
 });
